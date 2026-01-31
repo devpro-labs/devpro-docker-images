4c7340284d4e14ab748a78d23fb5d244d441beab
DEVAN CHAUHAN <155709255+Devan019@users.noreply.github.com>
2026-02-01 01:58:39 +0530

    [add] images

--- javascript-express/server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello from Base Node Express App!");
-});
-
-app.listen(3000, () => console.log("Base Express running at http://localhost:3000"));
