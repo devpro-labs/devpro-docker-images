4d93bd91cb0239e57e40c2df8a7711e43febf30c
DEVAN CHAUHAN <155709255+Devan019@users.noreply.github.com>
2026-03-26 23:46:29 +0530

    [fix] images

--- express-javascript/src/index.js
+++ express-javascript/src/index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import mongoose from "mongoose";
+import pkg from "pg";
+const { Client } = pkg;
+import {createClient} from "redis";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -12,6 +18,45 @@ app.get("/data", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Base service running on port ${PORT}`);
-});
+
+async function main() {
+  console.log("Base service is starting...");
+
+  //connect to mongodb
+  await mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("MongoDB connection successful");
+    })
+    .catch((err) => {
+      console.error("MongoDB connection error:", err);
+
+    });
+
+  //connect to postgresql
+  const client = new Client({
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DB,
+  });
+
+  await client.connect();
+  console.log("PostgreSQL connection successful");
+
+
+  //connect to redis
+  const redis = new createClient({
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  })
+  console.log("Redis client created", redis);
+
+
+
+  app.listen(PORT, () => {
+    console.log(`Base service running on port ${PORT}`);
+  });
+
+
+}
+
+main()
